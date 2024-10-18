79af12a8865b7464f32040657014df2bd03af067
Stephanie Gonzalez <gstephaniegonz@gmail.com>
2024-10-18 16:48:38 -0500

    added a search function for the websites

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 
+from os import close
 from tkinter import *
 from tkinter import messagebox
 from random import *
 import pyperclip
-
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     pas_entry.delete(0,END)
@@ -30,20 +31,48 @@ def save_data():
     website=web_entry.get()
     email_user=user_entry.get()
     password=pas_entry.get()
-    
-    if len(website)>0 and len(email_user)>0 and len(password)>0:
-        is_ok=messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail/User: {email_user} \nPassword: {password} Is it ok?")
-
-        if is_ok:
-            
-            with open("data_pas.txt", "a") as data_file:
-                data_file.write(f"Website: {website}, Email/User: {email_user}, Pas: {password}\n")
+    new_data={
+        website:{
+            "email":email_user,
+            "password":password
+        }
+    }
+    if len(website)==0 or len(email_user)==0 or len(password)==0:
+        warning_box=messagebox.showerror(title="Empty Fields", message="Sorry, there are empty files")
+        
+    else:    
+        try:
+            with open("data_pas.jason", "r") as data_file:
+                data=json.load(data_file)
+        except FileNotFoundError:
+            with open("data_pas.jason", "w") as data_file:
+                json.dump(new_data,data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data_pas.jason", "w") as data_file:
+                json.dump(data,data_file, indent=4)
+        finally:
             web_entry.delete(0,END)
             pas_entry.delete(0,END)
 
+    
+# ---------------------------- search for website ------------------------------- #        
+def search():
+    website=web_entry.get()
+    try:
+        with open("data_pas.jason") as data_file:
+            data=json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Warning", message="You have not created any password yet")
     else:
-        warning_box=messagebox.showerror(title="Empty Fields", message="Sorry, there are empty files")
-
+        if website in data:
+            email=data[website]["email"]
+            pas=data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {pas}")
+                    
+        else:
+            messagebox.showinfo(title="Not Found", message="website not found")   
+                
 
 
 
@@ -72,16 +101,16 @@ web_label=Label(text="Website: ")
 web_label.grid(column=0,row=1)
 
 
-web_entry=Entry(width=35)
-web_entry.grid(column=1,row=1,columnspan=2)
+web_entry=Entry(width=30)
+web_entry.grid(column=1,row=1,sticky="w")
 web_entry.focus()
 #. email/username
 
-user_label=Label(text="Email/Username: ")
+user_label=Label(text="Username: ")
 user_label.grid(column=0, row=2)
 
-user_entry=Entry( width=35)
-user_entry.grid(column=1, row=2, columnspan=2)
+user_entry=Entry( width=30)
+user_entry.grid(column=1, row=2, columnspan=2, sticky="w")
 user_entry.insert(0,"email@gmail.com")
 
 #.Password
@@ -89,15 +118,18 @@ user_entry.insert(0,"email@gmail.com")
 pas_label=Label(text="Password: ")
 pas_label.grid(column=0, row=3)
 
-pas_entry=Entry(width=21)
-pas_entry.grid(column=1,row=3)
+pas_entry=Entry(width=30)
+pas_entry.grid(column=1,row=3,sticky="w")
 
 #.generate password button
 
 button_generate=Button( text="Generate", command=generate_password)
 button_generate.grid(column=2,row=3)
 
+#.search button
 
+search_button=Button(text="search", width=7, command=search)
+search_button.grid(column=2, row=1)
 #.Add password button
 
 button_add=Button(width=36, text="Add Password", command=save_data)
